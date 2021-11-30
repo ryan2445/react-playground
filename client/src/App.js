387e5130058b013d5d7db0f9d3caf70fd3f12c87
Ryan Hoffman <ryan@snapdesk.app>
2021-11-30 15:19:42 -0800

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ryan's React Playground!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            Ryan's React Playground!
+            <div className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>button</div>
+        </header>
     </div>
   );
 }
