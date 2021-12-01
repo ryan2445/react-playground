dc79c24fa3c4789873b6352d0bb6e1f8f73e8c56
Ryan Hoffman <ryan@snapdesk.app>
2021-11-30 16:15:58 -0800

    Messing with components/data

--- api/NotesController/notes.js
+++ api/NotesController/notes.js
@@ -1,3 +1,8 @@
-exports.handler =  function(event, context, callback) {
-  callback(null, event)
+exports.handler =  async function(event, context, callback) {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(event)
+    }
+    
+    return response
 }
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,18 @@
 import './App.css';
+import React, { useState } from 'react'
+
+function NotesList() {
+    const [notes, setNotes] = useState(1)
+
+    return (
+        <div className="Notes-list">
+            <ul className="collection">
+            
+            <li className="collection-item">test</li>
+            </ul>
+        </div>
+    )
+}
 
 function App() {
   return (
@@ -6,6 +20,7 @@ function App() {
         <header className="App-header">
             Ryan's React Playground!
             <div className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>button</div>
+            <NotesList />
         </header>
     </div>
   );
