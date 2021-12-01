9e03b3f9cf40b91aa15b4b46b5952cb1e5c54de8
ryan2445 <ryan@snapdesk.app>
2021-11-30 21:32:08 -0800

    Formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,79 @@
-import './App.css';
-import { useState } from 'react'
-import { IconButton, TextField } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import CloseIcon from '@mui/icons-material/Close';
+import "./App.css";
+import { useState } from "react";
+import { IconButton, TextField } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
 
 function NotesList() {
-    var [notes, setNotes] = useState([])
-    var [editing, setEditing] = useState(false)
-    var [tempNote, setTempNote] = useState('')
+  var [notes, setNotes] = useState([]);
+  var [editing, setEditing] = useState(false);
+  var [tempNote, setTempNote] = useState("");
 
-    function keyPressed(key) {
-        if (key.keyCode == 13) {
-            setNotes([...notes, tempNote])
-            setEditing(false)
-        }
+  function keyPressed(key) {
+    if (key.keyCode == 13) {
+      setNotes([...notes, tempNote]);
+      setEditing(false);
     }
+  }
 
-    function textFieldChanged(event) {
-        setTempNote(event.target.value)
-    }
+  function textFieldChanged(event) {
+    setTempNote(event.target.value);
+  }
 
-    function clearTempNote() {
-        setTempNote('')
-        setEditing(false)
-    }
+  function clearTempNote() {
+    setTempNote("");
+    setEditing(false);
+  }
 
-    return (
-        <div>
-            <div>
-                { notes.map((note, i) => <div key={i}>{note}</div>) }
-            </div>
-            { editing && <div style={{display:"flex", justifyContent:'center', width:'340px'}}>
-                <TextField variant="outlined" autoFocus fullWidth onKeyUp={keyPressed} onChange={textFieldChanged}/>
-                <div style={{alignSelf:'center', paddingLeft:'5px'}}>
-                    <IconButton color="error" size="small" onClick={() => clearTempNote()}>
-                        <CloseIcon fontSize="small"></CloseIcon>
-                    </IconButton>
-                </div>
-            </div> }
-            <div style={{paddingTop:'15px'}}>
-                <IconButton color="primary" onClick={() => setEditing(true)} disabled={editing}>
-                    <AddIcon></AddIcon>
-                </IconButton>
-            </div>
+  return (
+    <div>
+      <div>
+        {notes.map((note, i) => (
+          <div key={i}>{note}</div>
+        ))}
+      </div>
+      {editing && (
+        <div
+          style={{ display: "flex", justifyContent: "center", width: "340px" }}
+        >
+          <TextField
+            variant="outlined"
+            autoFocus
+            fullWidth
+            onKeyUp={keyPressed}
+            onChange={textFieldChanged}
+          />
+          <div style={{ alignSelf: "center", paddingLeft: "5px" }}>
+            <IconButton
+              color="error"
+              size="small"
+              onClick={() => clearTempNote()}
+            >
+              <CloseIcon fontSize="small"></CloseIcon>
+            </IconButton>
+          </div>
         </div>
-    )
+      )}
+      <div style={{ paddingTop: "15px" }}>
+        <IconButton
+          color="primary"
+          onClick={() => setEditing(true)}
+          disabled={editing}
+        >
+          <AddIcon></AddIcon>
+        </IconButton>
+      </div>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
-            <div style={{paddingBottom:'15px'}}>
-                Notes List
-            </div>
-            <NotesList />
-        </header>
+      <header className="App-header">
+        <div style={{ paddingBottom: "15px" }}>Notes List</div>
+        <NotesList />
+      </header>
     </div>
   );
 }
