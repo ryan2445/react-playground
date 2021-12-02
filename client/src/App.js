d567a85a822ca6656815cebfab011eec7f7ffa81
Ryan Hoffman <ryan@snapdesk.app>
2021-12-02 14:16:06 -0800

    Frontend connected to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
-import { useState } from 'react'
-import { Button, IconButton, TextField } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { Button, IconButton, TextField, CircularProgress } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import CloseIcon from '@mui/icons-material/Close'
 
@@ -8,12 +8,32 @@ function NotesList() {
     var [notes, setNotes] = useState([])
     var [editing, setEditing] = useState(false)
     var [tempNote, setTempNote] = useState('')
+    var [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(
+            'https://9hdsbfgql2.execute-api.us-west-2.amazonaws.com/Prod/notes',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+            .then(response => response.json())
+            .then(data => {
+                setNotes(data.notes)
+                setLoading(false)
+            })
+    }, [])
 
     function keyPressed(key) {
         if (key.keyCode === 13) {
-            setNotes([...notes, tempNote])
+            var temp = [...notes, tempNote]
+            setNotes(temp)
             setEditing(false)
             setTempNote('')
+            saveNotes(temp)
         }
     }
 
@@ -26,40 +46,56 @@ function NotesList() {
         setEditing(false)
     }
 
-    function removeItem(index) {
-        if (notes.length === 1) return setNotes([])
+    function removeItem(toRemove) {
+        var temp = []
+        if (notes.length !== 1) temp = notes.filter(note => note !== toRemove)
+        setNotes(temp)
+        saveNotes(temp)
+    }
 
-        var temp = notes.splice(index, 1)
-
-        console.log(temp)
+    function saveNotes(notes) {
+        fetch(
+            'https://9hdsbfgql2.execute-api.us-west-2.amazonaws.com/Prod/notes',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ notes: notes })
+            }
+        )
     }
 
     return (
         <div>
             <div className="listItems">
                 <hr style={{ border: '1px solid grey' }}></hr>
-                {notes.map((note, i) => (
-                    <div key={i}>
-                        <div className="listItem">
-                            <div>{note}</div>
-                            <div
-                                style={{
-                                    alignSelf: 'center',
-                                    paddingLeft: '5px'
-                                }}
-                            >
-                                <IconButton
-                                    color="error"
-                                    size="small"
-                                    onClick={() => removeItem(i)}
+                {loading ? (
+                    <CircularProgress />
+                ) : (
+                    notes.map((note, i) => (
+                        <div key={i}>
+                            <div className="listItem">
+                                <div>{note}</div>
+                                <div
+                                    style={{
+                                        alignSelf: 'center',
+                                        paddingLeft: '5px'
+                                    }}
                                 >
-                                    <CloseIcon fontSize="small"></CloseIcon>
-                                </IconButton>
+                                    <IconButton
+                                        color="error"
+                                        size="small"
+                                        onClick={() => removeItem(note)}
+                                    >
+                                        <CloseIcon fontSize="small"></CloseIcon>
+                                    </IconButton>
+                                </div>
                             </div>
+                            <hr style={{ border: '1px solid grey' }}></hr>
                         </div>
-                        <hr style={{ border: '1px solid grey' }}></hr>
-                    </div>
-                ))}
+                    ))
+                )}
             </div>
             {editing && (
                 <div
