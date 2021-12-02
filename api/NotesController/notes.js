15d02b34c6a4f8e88ebf2f402116ba1c1e4e8940
Ryan Hoffman <ryan@snapdesk.app>
2021-12-02 12:11:14 -0800

    Adding notesController functions

--- api/NotesController/notes.js
+++ api/NotesController/notes.js
@@ -1,8 +1,57 @@
-exports.handler =  async function(event, context, callback) {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(event)
+const AWS = require('aws-sdk')
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+async function notesGet(event, context, callback) {
+    const params = {
+        TableName: 'system',
+        Key: {
+            pk: 'ID#ryan',
+            sk: 'NOTES'
+        }
     }
-    
-    return response
+
+    const response = await dynamodb.get(params).promise()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response.Item)
+    }
+}
+
+async function notesPost(event, context, callback) {
+    const body = JSON.parse(event.body)
+
+    const params = {
+        TableName: 'system',
+        Item: {
+            pk: 'ID#ryan',
+            sk: 'NOTES',
+            notes: body.notes
+        }
+    }
+
+    await dynamodb.put(params).promise()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Item created!')
+    }
+}
+
+exports.handler = async function (event, context, callback) {
+    const method = event.httpMethod
+
+    if (method == 'GET') {
+        return await notesGet(event, context, callback)
+    }
+
+    if (method == 'POST') {
+        return await notesPost(event, context, callback)
+    }
+
+    return {
+        statusCode: 404,
+        body: JSON.stringify('Not Found.')
+    }
 }
